a75f5e6d7475b65a88e79a1d4ad7b0c66860dd72
oniontherock <orionmartin049@gmail.com>
2026-07-31 13:47:12 -0400

    Fixed some issues with timing and such and added basic filtering

--- Teleoperator.py
+++ Teleoperator.py
@@ -25,17 +25,44 @@ def on_result(result, output_image, timestamp_ms):
 base_options = mediapipe.tasks.BaseOptions(model_asset_path = str(MODEL_PATH))
 options = vision.PoseLandmarkerOptions(base_options=base_options, running_mode = vision.RunningMode.LIVE_STREAM, result_callback = on_result)
 
+def calculate_alpha(hz):
+    pass
+
+class Filter:
+    def __init__(self, alpha):
+        self.val_filtered_prev = (None, None, None)
+        self.alpha = alpha
+        self.first_run = True
+
+    # does no filtering in basic filter type
+    def filter_value(self, val:tuple) -> tuple:
+        self.first_run = False
+        return val
+
+class LowPass(Filter):
+    def filter_value(self, val:tuple) -> tuple:
+
+        # we need to set a value for val_filtered_prev if we've never run before, so we assign it to val on the first run.
+        # this basically makes our first run's filtered value just be the unfilitered value, but just for the first run
+        if (self.first_run):
+            self.first_run = False
+            self.val_filtered_prev = val
+
+        val_filtered = (
+            (val[0] * self.alpha) + (self.val_filtered_prev[0] * (1.0 - self.alpha)),
+            (val[1] * self.alpha) + (self.val_filtered_prev[1] * (1.0 - self.alpha)),
+            (val[2] * self.alpha) + (self.val_filtered_prev[2] * (1.0 - self.alpha))
+            )
+        self.val_filtered_prev = val_filtered
+
+        return val_filtered
+
 # create window
 cv2.namedWindow("MyWindow")
 # declare and initialize video capture from default webcame, allows you to read webcam data on demand
 videoCapture = cv2.VideoCapture(0)
 
-# Get first frame and register if it works
-if videoCapture.isOpened():
-    # get whether the frame was captured successfully, and the actual frame if it did work
-    frameCapOk, frame = videoCapture.read()
-else:
-    frameCapOk = False
+frameCapOk, frame = videoCapture.isOpened(), None
 
 # wrist ID in mediapipe is 16
 WRIST_ID = 16
@@ -43,14 +70,16 @@ WRIST_ID = 16
 FIXED_ORIENT = [0.0, 3.14159, 0.0]  # tool pointing down (axis-angle, rad) — verify for your tool
 VIS_THRESHOLD = 0.5
 
-def lerp(t, lo, hi):  return lo + t * (hi - lo)
 def clamp(v, lo, hi): return max(lo, min(hi, v))
 
 def wrist_position_get(world_landmarks):
     wrist = world_landmarks[0][WRIST_ID]
     if wrist.visibility < VIS_THRESHOLD:
         return None
-    return (wrist.x, wrist.y, wrist.z)
+    return [wrist.x, wrist.y, wrist.z]
+
+def filter_wrist_position(wrist_position, filter:Filter):
+    return filter.filter_value(wrist_position)
 
 
 # this comes in after filter
@@ -74,7 +103,7 @@ def map_to_robot(d):
 
     return [-rz, rx, -ry, *FIXED_ORIENT]
 
-target = [0,0.5,0.5,0,0,]
+target = [0, 0.5, 0.5, 0.0, 3.14159, 0.0]
 
 def update_arm(robot_ip):
 
@@ -109,46 +138,59 @@ control_thread = threading.Thread(target=update_arm, args=(control_ip,))
 # so we only allow the control loop to run the single moment that the mediapipe loop runs/receives new data.
 previous_timestamp = -1
 
-with vision.PoseLandmarker.create_from_options(options) as landmarker:
+low_pass = LowPass(0.02)
+try:
+    with vision.PoseLandmarker.create_from_options(options) as landmarker:
 
 
-    control_thread.start()
+        control_thread.start()
 
-    start_time = time.perf_counter_ns()
-    while (frameCapOk):
+        start_time = time.perf_counter_ns()
+        while (frameCapOk):
 
-        # updates the window with the current frame, only ever runs if frameCapOk is true
-        cv2.imshow("MyWindow", frame) # type: ignore
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # type: ignore
-        mediapipe_image = mediapipe.Image(image_format = mediapipe.ImageFormat.SRGB, data = frame_rgb)
-        landmarker.detect_async(mediapipe_image, (time.perf_counter_ns() - start_time) // 1000)
+            # get next frame from webcam
+            frameCapOk, frame = videoCapture.read()
 
-        # get next frame from webcam
-        frameCapOk, frame = videoCapture.read()
+            if not frameCapOk:
+                break
 
-        with lock:
-            lm = pose["landmarks"]
-        if (lm) and (previous_timestamp != pose["timestamp"]):
-            previous_timestamp = pose["timestamp"]
-            wrist_position = wrist_position_get(lm)
-            if wrist_position is not None:
+            # updates the window with the current frame, only ever runs if frameCapOk is true
+            cv2.imshow("MyWindow", frame) # type: ignore
 
-                with (position_lock):
-                    target = map_to_robot(wrist_position)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # type: ignore
+            mediapipe_image = mediapipe.Image(image_format = mediapipe.ImageFormat.SRGB, data = frame_rgb)
+            landmarker.detect_async(mediapipe_image, (time.perf_counter_ns() - start_time) // 1000000)
+
+            with lock:
+                lm = pose["landmarks"]
+                ts = pose["timestamp"]
+            if (lm) and (previous_timestamp != ts):
+                previous_timestamp = ts
+                wrist_position = wrist_position_get(lm)
+                if wrist_position is not None:
+
+                    wrist_position = filter_wrist_position(wrist_position, low_pass)
+                    
+                    position_mapped = map_to_robot(wrist_position)
+
+                    with (position_lock):
+                        target = position_mapped
                 
 
-        # check if esc was pressed, if it was, break the loop
-        keyPress = cv2.waitKey(20)
-        if (keyPress == 27):
-            break
+            # check if esc was pressed, if it was, break the loop
+            keyPress = cv2.waitKey(1)
+            if (keyPress == 27):
+                break
+finally:
+    is_robot_running = False
+    if control_thread.is_alive():
+        control_thread.join()
 
-is_robot_running = False
-control_thread.join()
+    # close window and release webcam VideoCapture device
+    cv2.destroyWindow("MyWindow")
+    videoCapture.release()
 
-# close window and release webcam VideoCapture device
-cv2.destroyWindow("MyWindow")
-videoCapture.release()
 # kill rtde script-to-robot interation
 # print that the program is finished
 print("Program End")
