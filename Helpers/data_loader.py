87c304856e507faa0f1ce6bb966156d3bb625b9a
Orion Martin <orionmartin049@gmail.com>
2026-09-06 16:05:24 -0400

    Made it so data loader can optionally unpack

--- Helpers/data_loader.py
+++ Helpers/data_loader.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def load_data(name, skip_count):
+def load_data(name, skip_count, unpack=True):
 
     data = np.loadtxt(
         "Data/" + name + ".txt",
         delimiter=',',
         skiprows=skip_count,
-        unpack=True,
+        unpack=unpack,
     )
 
     return data
