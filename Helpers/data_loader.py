473bc045d1899ae3ca0eb91617ccb99156a9fdf9
Orion Martin <orionmartin049@gmail.com>
2026-09-08 08:17:40 -0400

    refactored data_filterer so it's a part of data_loader and returns a dict instead

--- Helpers/data_loader.py
+++ Helpers/data_loader.py
@@ -10,3 +10,21 @@ def load_data(name, skip_count, unpack=True):
     )
 
     return data
+
+# dict loading functions
+def data_convert_to_dict(data, timestamp_index=-1):
+
+    data_as_dict = {}
+
+    for i in range(len(data)):
+        data_as_dict[round(data[i][timestamp_index])] = [data[i][0]]
+
+    return data_as_dict
+
+def data_load_as_dict(file_name, timestamp_index=-1):
+    pos_data = load_data(file_name, 1, False)
+
+    data_as_dict = data_convert_to_dict(pos_data, timestamp_index)
+
+    return data_as_dict
+
