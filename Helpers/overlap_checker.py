3a90e13359247757ed7c881956553160cc2aa52f
Orion Martin <orionmartin049@gmail.com>
2026-09-11 09:27:49 -0400

    Added overlap checker to check lots of data at once and commented out some helper print statements in Teleoperator

--- Helpers/overlap_checker.py
+++ Helpers/overlap_checker.py
@@ -2,30 +2,35 @@ import data_loader
 
 skip_count = 31
 
-data = data_loader.load_data("timestamps__2026_09_08__10h29m40s", skip_count, False)
+def process_overlaps(filename, preexisting_overlap_dict):
 
-size = len(data)
+    data = data_loader.load_data(filename, skip_count, False)
+
+    size = len(data)
+
+    for data_cur_ind in range(size-1):
+
+        data_cur = data[data_cur_ind]
+        data_next = data[data_cur_ind+1]
+
+        overlap = round(data_cur[3] - data_next[0])
+
+        if overlap in preexisting_overlap_dict:
+            preexisting_overlap_dict[overlap] += 1
+        else:
+            preexisting_overlap_dict[overlap] = 1
 
 
-overlaps = [0,0,0,0,0,0,0,0,0,0]
-avg_overlap = 0
-
-for data_cur_ind in range(size-1):
-
-    data_cur = data[data_cur_ind]
-    data_next = data[data_cur_ind+1]
-
-    overlap = round(data_cur[3] - data_next[0])
-
-    avg_overlap += overlap
-
-    overlaps[overlap] += 1
 
 
-avg_overlap /= size-1
+
+overlaps:dict = {0:0}
+
+for i in range(1, 15):
+    process_overlaps(f"timestamps{i}", overlaps)
+
+sorted_overlaps = dict(sorted(overlaps.items()))
 
 print(
-    "Data points: ", size, "\n",
-    "Average overlap: ", avg_overlap, "\n",
-    *(f"overlap count for {i}: {overlaps[i]}\n" for i in range(len(overlaps)))
+    *(f"overlap count for {key}: {sorted_overlaps[key]}\n" for key in sorted_overlaps)
     )
