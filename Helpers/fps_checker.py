f63d2239455c76816d01eb69732857ceb0e662ae
Orion Martin <orionmartin049@gmail.com>
2026-09-11 15:34:52 -0400

    Made it so the FPS checker can check ranges of data sources in the same run

--- Helpers/fps_checker.py
+++ Helpers/fps_checker.py
@@ -1,29 +1,40 @@
 import data_loader
 
-data = data_loader.load_data("sampler6__2026_09_02__12h12m01s", 1)
+# NOTE, we exclude videos 6-11 in this because they use a video that is producing very odd results due to lack of visiblity on hand, and just generally bad recording procedures.
 
-ts1 = data[3]
+for j in range(1, 15):
 
-fps = []
-diff = []
+    if (j in range(6,11)):
+        continue
 
-size = len(ts1)
+    name = f"timestamps{j}"
 
-for i in range(size-1):
-    fps.append(1.0/((ts1[i+1]-ts1[i])*1e-3))
-    diff.append(ts1[i+1]-ts1[i])
+    data = data_loader.load_data(name, 31)
 
-avg = 0
-for i in range(size-1):
-    avg += diff[i]
+    ts1 = data[3]
 
-avg /= (size - 1)
+    # NOTE, Timestamps1-5 are TestVideo. Timestamps 6-10 are TestVideo2. Timestamps 11-15 are hand_held
 
-final_fps = 1000/avg
+    fps = []
+    diff = []
 
-print(
-    "Data points: ", size, "\n",
-    "Average FPS: ", final_fps, "\n",
-    "Min Diff: ", min(diff), " ", diff.index(min(diff)), "\n",
-    "Max Diff: ", max(diff), " ", diff.index(max(diff)), "\n"
-    )
+    size = len(ts1)
+
+    for i in range(size-1):
+        fps.append(1.0/((ts1[i+1]-ts1[i])*1e-3))
+        diff.append(ts1[i+1]-ts1[i])
+
+    avg = 0
+    for i in range(size-1):
+        avg += diff[i]
+
+    avg /= (size - 1)
+
+    final_fps = 1000/avg
+
+    print(
+        f"Data points in {name} : ", size, "\n",
+        "Average FPS: ", final_fps, "\n",
+        "Min Diff: ", min(diff), " ", diff.index(min(diff)), "\n",
+        "Max Diff: ", max(diff), " ", diff.index(max(diff)), "\n"
+        )
